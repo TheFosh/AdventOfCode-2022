0e034125beafa444672b2dfd565d5dc766cde3eb
Jake Swanson <jakeswan213@gmail.com>
2023-11-22 11:40:04 -0600

    Reading input

--- AdventOfCode_Day6/AdventOfCode_Day6/Runner.cpp
+++ AdventOfCode_Day6/AdventOfCode_Day6/Runner.cpp
@@ -1,6 +1,23 @@
+#include <iostream>
+#include <fstream>
 using namespace std;
 
+string const FILEPATH = "PuzzleInput.txt";
+
 int main(){
+
+	ifstream inFile;
+
+	try {
+		inFile.open(FILEPATH);
+		string errorMessage;
+		if (inFile.fail())
+			errorMessage = FILEPATH + "not found.";
+			throw errorMessage;
+	}
+	catch (string e) {
+		cout << e << endl;
+	}
 
 
 
